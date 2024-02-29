a2aa9c443a8b06c605dcf0dbe20eab3f412e7e68
suhankins <punishedbernadetta@niuitmo.ru>
2024-03-01 01:24:46 +0300

    switched to vercel
    cloudflare workers can't access spotify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: cloudflare(),
+  output: 'server',
+  adapter: vercel()
 });
