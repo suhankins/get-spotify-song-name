b60f7ad9ccdd7300a1662e8235f3ec670008de5e
suhankins <punishedbernadetta@niuitmo.ru>
2024-11-03 18:40:11 +0400

    ported CopyButton to use Lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+import lit from '@astrojs/lit';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
+  integrations: [lit()],
 });
